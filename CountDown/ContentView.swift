2ff27fec4f1fd1a337c7c12eca858e76d28edd39
Boom PT <37561295+boompikachu@users.noreply.github.com>
2020-04-03 11:04:21 +0700

    Initial Commit

---
+++ CountDown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CountDown
+//
+//  Created by Patthanat Thanintantrakun on 3/4/20.
+//  Copyright © 2020 Patthanat Thanintantrakun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
